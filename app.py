e8e33a2d309062df4b02d60b783f7fbf67f55eac
Grant Bellar <grbellar@gmail.com>
2025-02-11 16:53:41 -0600

    Add real ip comparison and check for proxies

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask, jsonify, request
 from flask_httpauth import HTTPTokenAuth
 from functions import docx_to_txt, pdf_to_markdown
 import os
-
+import ipaddress
 
 app = Flask(__name__)
 auth = HTTPTokenAuth(scheme='Planetary')
-
+PROXY_HEADER_TYPES = [
+        'True-Client-Ip',
+        'X-Forwarded-For',
+        'X-Real-Ip', 
+        'CF-Connecting-Ip',
+        'X-Client-Ip'
+    ]
+TRUSTED_PROXIES = os.getenv('TRUSTED_PROXIES').split(',')
+print(TRUSTED_PROXIES)
+WHITELISTED_IPS = [ipaddress.ip_address(ip) for ip in os.getenv('WHITELISTED_IPS').split(',')]
 
 @auth.verify_token
 def verify_token(token):
@@ -14,15 +23,30 @@ def verify_token(token):
         return True
     return False
 
+
+def allowed_ip(request):
+    if request.remote_addr in TRUSTED_PROXIES:
+        for proxy_type in PROXY_HEADER_TYPES:
+            if proxy_type in request.headers:
+                try:
+                    client_ip = ipaddress.ip_address(request.headers[proxy_type])
+                    return client_ip in WHITELISTED_IPS
+                except Exception as e:
+                    pass
+    try:
+        client_ip = ipaddress.ip_address(request.remote_addr)
+        return client_ip in WHITELISTED_IPS
+    except Exception as e:
+        return False
+
+
+
 @app.post("/convert-to-text")
 @auth.login_required
 def convert():
-    print(request.headers)
 
-    # TODO: Better IP checking. Especially if behind a proxy this won't work.
-    request_ip = request.remote_addr
-    if request_ip != os.getenv('WHITELISTED_IP'):
-        return jsonify({"error": "Invalid IP address"}), 401
+    if not allowed_ip(request):
+        return jsonify({"error": "Unauthorized"}), 401
     
     # Check if content-type is application/json
     if not request.is_json:
